0c5860ca19c5e2604d5ea869f5403c3677b90eb3
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-01-07 18:42:29 +0100

    add item ex

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,28 @@ var addBtn = document.getElementById('addElem');
 var getList = document.getElementById('lista');
 var newElem = document.createElement('li');
 
+//opcja 1
+// addElem.addEventListener('click', function() {
+// 	itemNumber += 1;
+// 	getList.innerHTML += '<li>item ' + itemNumber + '</li>';
+// })
+
+//opcja 2
+var itemLength = getList.getElementsByTagName('li');
+
 addElem.addEventListener('click', function() {
-	newElem.innerHTML = 'item ' + itemNumber;
-	getList.appendChild(newElem);
+	getList.innerHTML += '<li>item ' + itemLength.length + '</li>';
 })
+
+
+//opcja 3
+// addElem.addEventListener('click', function() {
+// 	itemNumber += 1;
+// 	newElem.innerHTML = 'item ' + itemNumber;
+// 	getList.appendChild(newElem);
+// })
+
+//??Dlaczego po kliknięciu nie wykonuje się dodatkowy appendChild?
+
 
 
